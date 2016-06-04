6da6d7890e64a4b573a3014b41ace2b38c5bbf24
Jean-Loup Tastet <jean-loup.tastet@epfl.ch>
2016-06-04 20:53:41 +0200

    Support new file format (transposed).

--- post.R
+++ post.R
@@ -5,6 +5,6 @@ vac.scatterplot <- function(filename) {
 
   file <- h5file(filename,"r")
   dset <- file["means"]
-  dat <- data.frame(β = c(dset[1,]), mean = c(dset[2,]), t = c(dset[3,]))
+  dat <- data.frame(β = c(dset[,1]), mean = c(dset[,2]), t = c(dset[,3]))
   dat
 }
