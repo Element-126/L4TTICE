5dd7e5702f530ca7034f6ac04a8ab2988870db44
Jean-Loup Tastet <jean-loup.tastet@epfl.ch>
2016-06-06 00:44:53 +0200

    Add plot for the scaling.

--- post.R
+++ post.R
@@ -1,11 +1,19 @@
 library(h5)
 library(ggplot2)
 
-vac.scatterplot <- function(filename) {
+## Utility function to read a HDF5 file and create a data frame with it.
+read.h5 <- function(filename) {
 
   file <- h5file(filename,"r")
   dset <- file["means"]
   dat <- data.frame(β = c(dset[,1]), mean = c(dset[,2]), t = c(dset[,3]))
+  dat
+}
+
+## Plots each field mean value for each temperature as a dot.
+mean.scatterplot <- function(filename) {
+
+  dat <- read.h5(filename)
 
   dev.new(width = 5.5, height = 4.5)
   par(mar = c(4,5,1.5,1.5) + 0.1)
@@ -14,4 +22,16 @@ vac.scatterplot <- function(filename) {
          scale_x_log10(expression(beta), breaks = breaks, minor_breaks = NULL) +
          theme(axis.text.x = element_text(angle = -45, vjust = 0.5)) +
          ylab(expression(symbol("\341")~Phi[H]~symbol("\361")[list(tau,x)]))
+}
+
+## Log-log plot of the computation time per configuration as a function of the lattice size Ni.
+scaling <- function(filename) {
+
+  dat <- read.table(filename, header = TRUE)
+  dev.new(width = 4.5, height = 3.5)
+  par(mar = c(4,5,1.5,1.5) + 0.1)
+  breaks = 16*(1:5)
+  ggplot(data = dat, aes(x = Ni, y = t)) + geom_point() +
+         geom_errorbar(aes(ymin = t-sd, ymax = t+sd), width=0.01) +
+         scale_x_log10(expression(N[i]), breaks = breaks) + scale_y_log10("t [s]")
 }
